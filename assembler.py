04bd40fd4343e542abef5137a04da27ca3a7c255
Mustafa Tolga Akbaba <76005690+mtlgkbb20@users.noreply.github.com>
2023-12-01 16:33:09 +0300

    Assembler done

--- assembler.py
+++ assembler.py
@@ -1,81 +1,68 @@
-OPCODES = {
-    'ADD': '00000',
-    'AND': '00001',
-    'NAND': '00010',
-    'NOR': '00011',
-    'ADDI': '00100',
-    'ANDI': '00101',
-    'LD': '00110',
-    'ST': '00111',
-    'CMP': '01000',
-    'JUMP': '01001',
-    'JE': '01010',
-    'JA': '01011',
-    'JB': '01100',
-    'JAE': '01101',
-    'JBE': '01110'
-    # Add more as needed
-}
+def turnToBinary(decimalNum):
+    binNum = format(decimalNum, '0{}b'.format(4))
+    return binNum
 
-REGISTER_PREFIX = 'R'
-MAX_REGISTER_NUMBER = 15
-IMMEDIATE_BITS = 10  # Assuming 10-bit immediate values for ADDI and JUMP
 
-def extract_register(register_str):
-    try:
-        register_num = int(register_str[len(REGISTER_PREFIX):])
-        if 0 <= register_num <= MAX_REGISTER_NUMBER:
-            return format(register_num, '04b')
-    except ValueError:
-        pass
-    raise ValueError(f"Invalid register: {register_str}")
-
-def extract_immediate(immediate_str):
-    try:
-        immediate_value = int(immediate_str)
-        return format(immediate_value, f'0{IMMEDIATE_BITS}b')
-    except ValueError:
-        raise ValueError(f"Invalid immediate value: {immediate_str}")
 
 def assemble(instruction):
-    parts = instruction.split()
-    opcode = OPCODES.get(parts[0])
-    if opcode is None:
-        raise ValueError(f"Invalid opcode: {parts[0]}")
+    opcode = {
+        'ADD': '0000',
+        'ADDI': '0001',
+        'LD': '0011',
+        'ST': '0100',
+        'CMP': '0101',
+        'JE': '0110',
+        'JA': '0111',
+        'JB': '1000',
+        'JAE': '1001',
+        'JBE': '1010',
+        'AND': '1011',
+        'NOR': '1100',
+        'ANDI': '1101',
+        'NAND': '1110',
+        'JUMP': '0010'
+        # Add more opcodes as needed
+    }
+    
+    # Tokenize instruction
+    parts = instruction.split(" ")
+    op = parts[0]
+    operands = parts[1:]
+    
+    # Get opcode and convert to binary
+    binary_instruction = opcode[op]
+    
+    # Convert operands to binary
+    for operand in operands:
+        if operand.startswith('R'):
+            if len(operand)==3:
+                register_num = int(operand[1])
+                binary_instruction += turnToBinary(register_num)
+            else:
+                register_num = int(operand[1:3])
+                binary_instruction += turnToBinary(register_num)
+        else:
+            # Assume immediate value
+            immediate_val = int(operand)
+            binary_instruction += turnToBinary(immediate_val)
+    
+    # Pad the binary instruction to 18 digits
+    binary_instruction = binary_instruction.ljust(18, '0')    
+    return binary_instruction
 
-    if parts[0] in ['JUMP', 'JE', 'JA', 'JB', 'JAE', 'JBE']:
-        if len(parts) != 2:
-            raise ValueError(f"Invalid format for {parts[0]} instruction")
-        address = extract_immediate(parts[1])
-        machine_code = opcode + address
-    else:
-        if len(parts) != 4:
-            raise ValueError(f"Invalid format for {parts[0]} instruction")
-        dest_reg = extract_register(parts[1])
-        src_reg1 = extract_register(parts[2])
-        src_reg2 = extract_register(parts[3])
-        machine_code = opcode + dest_reg + src_reg1 + src_reg2
+# Read instructions from a file
+with open('input.txt', 'r') as file:
+    instructions = file.readlines()
 
-    return format(int(machine_code, 2), '05X')
+# Process each instruction
+binary_instructions = []
+for instruction in instructions:
+    binary_instructions.append(assemble(instruction.strip()))
 
-def main():
-    assembly_code = [
-        'ADD R5, R0, R2',
-        'ADDI R3, R1, 12',
-        'JUMP 3',
-        'LD R5, 12',
-        'ST R3, 15',
-        'CMP R2, R4',
-        'JE 8',
-        # Add more instructions
-    ]
+# Convert binary instructions to hexadecimal
+hex_instructions = [hex(int(inst, 2))[2:].zfill(5) for inst in binary_instructions]
 
-    for instruction in assembly_code:
-        try:
-            machine_code = assemble(instruction)
-            print(f"{instruction} -> {machine_code}")
-        except ValueError as e:
-            print(f"Error in '{instruction}': {e}")
-
-if __name__ == "__main__":
-    main()
+# Write hexadecimal instructions to a file
+with open('output.txt', 'w') as file:
+    file.write("v2.0 raw\n")
+    file.write(' '.join(hex_instructions))
